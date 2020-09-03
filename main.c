a0fa77a5e8c0adb2e09ba037ebae90f17e725ff0
jjm07 <jjm7410@psu.edu>
2020-09-03 19:18:18 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+//Author: Joshua McIntyre jjm7410@psu.edu
+
+
+#include <stdio.h>
+#include<readline/readline.h>
+#include<stdlib.h>
+
+
+int main(void) {
+  char *tempstr = readline("Enter temperature in celsius")
+  double tempf = atof(tempc);
+
+  tempf = tempf*9/5+32
+  printf("%f° in celsius is equivalent to %f° fahrenheit.\n", temp, 101.3)
+
+  return 0;
+}
